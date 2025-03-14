c999b9ae969a392c5776eeaf4ca4499053365cab
Faraaz Ahmad <faraaz98@live.com>
2025-03-14 10:09:42 +0000

    feat: add youtube timestamps

--- app/javascript/pages/Submission/Show.vue
+++ app/javascript/pages/Submission/Show.vue
@@ -3,18 +3,18 @@
 
   <p v-if="flash.notice" class="notice">{{ flash.notice }}</p>
 
-  <div class="container mx-auto flex flex-col gap-8">
+  <div class="container mx-auto flex flex-col gap-8 mt-8">
     <div class="flex flex-row items-center justify-between">
       <div>
-        <Link href="/submissions"><h2 class="text-sm">Submissions</h2></Link>
-        <h1 class="text-2xl font-black">{{ submission.title }}</h1>
+        <Link href="/submissions"><h2 class="text-md">Submissions</h2></Link>
+        <h1 class="text-3xl font-black">{{ submission.title }}</h1>
       </div>
 
       <div class="flex flex-row gap-4">
-        <Link class="bg-white rounded shadow border border-slate-200 hover:bg-slate-50 transition px-4 py-2" :href="`/submissions/${submission.id}/edit`">Edit</Link>
+        <Link class="bg-white rounded shadow border border-slate-300 hover:bg-slate-200 transition px-4 py-2" :href="`/submissions/${submission.id}/edit`">Edit</Link>
         <Link
           :href="`/submissions/${submission.id}`"
-          class="bg-rose-100 rounded shadow border border-rose-200 text-rose-800 hover:bg-slate-50 transition px-4 py-2"
+          class="bg-rose-100 rounded shadow border border-rose-300 text-rose-800 hover:bg-rose-200 transition px-4 py-2"
           as="button"
           method="delete"
         >
@@ -24,10 +24,13 @@
     </div>
 
     <div class="grid grid-cols-5 gap-8">
-      <iframe class="col-span-3 rounded shadow w-full h-150" :src="getYtEmbedLink(submission)" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen v-if="submission.submission_type === 'youtube'"></iframe>
+      <iframe class="col-span-3 rounded shadow w-full h-150" :src="`${getYoutubeEmbedURL(submission)}?start=${videoStart}&autoplay=${autoplay}`" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen v-if="submission.submission_type === 'youtube'"></iframe>
       <iframe class="rounded shadow col-span-3 w-full h-screen" :src="submission.url" v-else></iframe>
-      <div class="col-span-2 bg-white border border-slate-200 shadow p-4 rounded">
-        Hello there
+      <div id="content" class="h-150 overflow-y-auto col-span-2 bg-white border border-slate-300 shadow p-8 rounded flex flex-col gap-4">
+        <h2 class="font-black text-xl text-slate-800 mb-4">Top 10 highlights from video:</h2>
+        <div @click="() => setVideoStartTime(point)" v-for="point in parsedSubmission(submission)" class="cursor-pointer border border-slate-200 rounded bg-slate-50 px-8 py-4 hover:bg-indigo-100 hover:border-indigo-600 transition hover:shadow">
+          {{point['text']}}
+        </div>
       </div>
     </div>
   </div>
@@ -35,16 +38,26 @@
 
 <script setup lang="ts">
 import { Head, Link } from '@inertiajs/vue3'
-import { computed } from 'vue';
+import { ref, computed } from 'vue';
 import Submission from './Submission.vue'
 import { SubmissionType } from './types'
+import VueMarkdown from 'vue-markdown-render'
+import { parse } from 'yaml';
+
+let videoStart = ref(0);
+let autoplay = ref(0);
 
 const { submission, flash } = defineProps<{
   submission: SubmissionType
   flash: { notice?: string }
 }>()
 
-function getYtEmbedLink(submission) {
+function setVideoStartTime(point) {
+  videoStart.value = Math.round(point['start']);
+  autoplay.value = 1;
+}
+
+function getYoutubeEmbedURL(submission) {
   const urlObj = new URL(submission.url)
   const videoId = urlObj.searchParams.get('v')
   const embedVideoId = `https://www.youtube-nocookie.com/embed/${videoId}`;
@@ -52,14 +65,11 @@ function getYtEmbedLink(submission) {
   return embedVideoId;
 }
 
-const pdfPages = computed(() => {
-  return JSON.parse(submission.content).pages
-})
+function parsedSubmission(submission) {
+  return parse(submission.content);
+}
 
 </script>
 
-<style scoped>
-.notice {
-  color: green;
-}
+<style>
 </style>
